175fb204f51da77b6647bfef35010e54d44f93d3
prior <prior@cracklabs.com>
2012-04-06 11:55:36 -0400

    dry up namespacing

--- gtw/__init__.py
+++ gtw/__init__.py
@@ -1,1 +1,4 @@
-
+from organizer import Organizer
+from webinar import Webinar
+from session import Session
+from registrant import Registrant
--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 
 setup(
     name='gtwpy',
-    version='1.0.0',
+    version='1.0.1',
     description='Python GoToWebinar (GTW) REST API Wrapper',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/pygtw',
-    download_url='https://github.com/prior/gtwpy/tarball/v1.0.0',
+    download_url='https://github.com/prior/gtwpy/tarball/v1.0.1',
     packages=['gtw'],
     install_requires=[
         'nose==1.1.2',
-        'sanetime==3.4'],
-    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.4'],
+        'sanetime==3.4.1'],
+    dependency_links = ['https://github.com/prior/sanetime/tarball/v3.4.1'],
 )
