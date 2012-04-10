436083f9eb1357817d3949927823cb5bf1dfd37a
prior <prior@cracklabs.com>
2012-04-09 20:49:42 -0400

    forgot version bump

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='gtwpy',
-    version='1.0.3',
+    version='1.1.0',
     description='Python GoToWebinar (GTW) REST API Wrapper',
     author='Michael Prior',
     author_email='prior@cracklabs.com',
     url='https://github.com/prior/pygtw',
-    download_url='https://github.com/prior/gtwpy/tarball/v1.0.3',
+    download_url='https://github.com/prior/gtwpy/tarball/v1.1.0',
     packages=['gtw'],
     install_requires=[ 'nose==1.1.2' ]
 )
